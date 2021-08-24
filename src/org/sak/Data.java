404fb70088ecba3e9fa99e55be13bd8919f733b8
MohanaSa <mohanavino98@gmail.com>
2021-08-24 12:05:09 +0530

    loadUrl Done

--- src/org/sak/Data.java
+++ src/org/sak/Data.java
@@ -1,41 +1,21 @@
 package org.sak;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class Data {
+	
+	public void loadUrl() {
+
+System.out.println("Load Url");
+	}
+	
+	
 	public static void main(String[] args) {
-		Student s = new Student();
-		s.setAge(20);
-		s.setName("Sathish");
-		s.setPhone(9085637485l);
 		
-		Student s1 = new Student();
-		s1.setAge(30);
-		s1.setName("Suresh");
-		s1.setPhone(908675885l);
-		
-		Student s2 = new Student();
-		s2.setAge(25);
-		s2.setName("Varun");
-		s2.setPhone(809756575l);
-		
-		List<Student> li = new ArrayList<Student>();
-		li.add(s);
-		li.add(s1);
-		li.add(s2);
-		
-		for(int i=0;i<li.size();i++) {
-			System.out.println("Age is "+li.get(i).getAge());
-			System.out.println("Name is "+li.get(i).getName());
-			System.out.println("Phone is "+li.get(i).getPhone());
-			
-			
-			
-		}
-		}
-		
+		Data d = new Data();
+		d.loadUrl();
 		
 	}
-
+	
+	
+}
 
